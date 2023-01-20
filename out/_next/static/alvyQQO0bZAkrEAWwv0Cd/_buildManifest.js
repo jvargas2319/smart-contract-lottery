939b09cbc03b1db2bb889ab625ecc105e69c6012
trang-john <j.vargas2319@gmail.com>
2023-01-19 21:06:38 -0800

    added out folder

---
+++ out/_next/static/alvyQQO0bZAkrEAWwv0Cd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/959-747014926bba6249.js","static/chunks/pages/index-824f68037ce0c17c.js"],"/_error":["static/chunks/pages/_error-409f831d3504c8f5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import "../styles/globals.css"
 import { MoralisProvider } from "react-moralis"
 import { NotificationProvider } from "web3uikit"
+import "../styles/globals.css"
 
 export default function App({ Component, pageProps }) {
     return (
